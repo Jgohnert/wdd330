06a421868e923926127c7eb5652305f427d74ec2
Jgohnert <goh22001@byui.edu>
2024-09-20 16:46:43 -0600

    added css and added JS for the link to my W01 notes

--- scripts/activity-links.js
+++ scripts/activity-links.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://jgohnert.github.io/wdd330/';
+const mainURL = 'https://jgohnert.github.io/wdd330/';
 const linksURL = 'https://jgohnert.github.io/wdd330/data/links.json';
 
 const allActivities = document.querySelector('#activity');
@@ -14,12 +14,25 @@ async function getActivityLinks() {
 const displayActivity = (activities) => {
     activities.forEach((activity) => {
         let activityList = document.createElement('li');
-        let activityTitle = document.createElement('p');
-        lessonTitle.textContent = `week ${activity.week} - `;
+        let activityTitle = document.createTextNode(`Week ${activity.week} - `);
 
         activityList.appendChild(activityTitle);
+
+        activity.links.forEach((link) => {
+            let linkList = document.createElement('a');
+            linkList.href = link.url.startsWith('http') ? link.url : mainURL + link.url;
+            linkList.textContent = link.title;
+
+            if (!link.url.endsWith('.docx')) {
+                anchor.target = '_blank';
+            }
+
+            activityList.appendChild(linkList);
+
+        });
+
+        allActivities.appendChild(activityList);
     });
 }
-
 
 getActivityLinks()
