40dab223f56d999153d3d9385cc6c0a4b6c8d6b2
Jgohnert <goh22001@byui.edu>
2024-11-22 14:38:41 -0700

    All data, html, and most images have been added to the COLUTYMA project. And W10 reading notes are complete.

---
+++ colutyma/scripts/headerfooter.js
@@ -0,0 +1,32 @@
+async function renderWithTemplate(template, parentElement, data, callback, position = "afterbegin", clear = true) {
+    if (clear) {
+  
+      parentElement.innerHTML = "";
+    }
+    const htmlString = await template(data);
+    parentElement.insertAdjacentHTML(position, htmlString);
+    if (callback) {
+      callback(data);
+    }
+}
+
+function loadTemplate(path) {
+    return async function () {
+      const res = await fetch(path);
+      if (res.ok) {
+        const html = await res.text();
+        return html;
+      }
+    };
+}
+
+function loadHeaderFooter() {
+    const headerTemplate = loadTemplate("data/partials/header.html");
+    const footerTemplate = loadTemplate("data/partials/footer.html");
+    const parentHeader = document.querySelector(".main-header");
+    const parentFooter = document.querySelector(".main-footer");
+    renderWithTemplate(headerTemplate, parentHeader);
+    renderWithTemplate(footerTemplate, parentFooter);
+}
+
+loadHeaderFooter();
