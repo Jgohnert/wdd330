3f5d19f24dbece1d9f3e067a4d0ba4ef24be72f1
Jgohnert <goh22001@byui.edu>
2024-11-29 20:49:20 -0700

    Movie list page displays the films on the movies.json file

--- colutyma/scripts/movieList.js
+++ colutyma/scripts/movieList.js
@@ -9,12 +9,16 @@ async function movieListTemplate() {
     movies.forEach((movie) => {
         template += `
         <div>
+        <a href="#">
           <h2>${movie.title}</h2>
           <img src="${movie.poster}" alt="image of ${movie.title} poster">
           <p>${movie.rating}</p>
           <p>Genre: ${movie.genre}</p>
           <p>Synopsis</p>
-          <button>span arrow</button>
+          <div class="synopsis">
+            <button type="button" class="synopsis-button"></button>
+            <p>${movie.synopsis}</p>
+          </div></a>
           <a href="#">Get</a>
         </div>
         `;
@@ -27,6 +31,17 @@ async function movieListTemplate() {
 async function displayMovieList() {
     const allMovies = await movieListTemplate();
     filmList.innerHTML = allMovies;
+
+    // open and close synopsis
+    const synopsisButtons = document.querySelectorAll(".synopsis-button");
+    const synopses = document.querySelectorAll(".synopsis");
+
+    synopsisButtons.forEach((synopsisButton, index) => {
+        synopsisButton.addEventListener("click", () => {
+            synopses[index].classList.toggle("open");
+            synopsisButton.classList.toggle("open");
+        });
+    });
 }
 
 displayMovieList();
