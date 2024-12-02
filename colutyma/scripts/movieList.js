f2711818486d2bc1bc691dc0faf1e560cc4a7ffa
Jgohnert <goh22001@byui.edu>
2024-12-02 14:39:00 -0700

    Sort by date and rating is done. movies still need to be rerendered.

--- colutyma/scripts/movieList.js
+++ colutyma/scripts/movieList.js
@@ -1,9 +1,10 @@
 import { fetchMovies } from "./getdata.js"
 
 const filmList = document.querySelector("#film-list");
+const movies = await fetchMovies();
 
+// This is the template of for the movies that display on the movie list page.
 async function movieListTemplate() {
-    const movies = await fetchMovies();
     let template = "";
 
     movies.forEach((movie) => {
@@ -29,15 +30,47 @@ async function movieListTemplate() {
     return template;
 }
 
+// This function displays all the movies on the movie list page.
 async function displayMovieList() {
     const allMovies = await movieListTemplate();
     filmList.innerHTML = allMovies;
 
     // open and close synopsis
     expandInfo();
+    sortMovieList();
+}
 
-    const 
-    
+// Get's the number of full stars on each movie from the movie list
+function getNumerOfStars(stars) {
+  return stars.split("★").length - 1;  // This will return the count of "★"
+}
+
+// Displays the ordered list depending on how the user
+// wants the movies sorted.
+async function sortMovieList() {
+
+  const sortByName = document.querySelector(".sortby-name");
+  const sortByRating = document.querySelector(".sortby-rating");
+  const sortByNew = document.querySelector(".sortby-new");
+
+  sortByName.addEventListener("click", () => {
+    movies.sort((a, b) => {
+      return a.title.localeCompare(b.title);
+    });
+    displayMovieList();
+  });
+
+  sortByRating.addEventListener("click", () => {
+    movies.sort((a, b) => {
+
+      const rA = getNumerOfStars(a.rating);
+      const rB = getNumerOfStars(b.rating);
+
+      return rB - rA;
+    });
+    displayMovieList();
+  });
+
 }
 
 // Expands some of the info of each movie on the movie list page.
