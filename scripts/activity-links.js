b3f6e426a9cc726208334f84f83a8d8f7797f5d5
Jgohnert <goh22001@byui.edu>
2026-04-06 11:11:47 -0600

    changed url title

--- scripts/activity-links.js
+++ scripts/activity-links.js
@@ -6,36 +6,58 @@ const allActivities = document.querySelector('#activity');
 async function getActivityLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    const activities = data.lessons;
+    const webActivities = data.webclasses;
+    const codeActivities = data.codeclasses;
 
-    displayActivity(activities);
+    displayWebActivity(webActivities);
+    displayCodeActivity(codeActivities);
 }
 
-const displayActivity = (activities) => {
+const displayCodeActivity = (activities) => {
     activities.forEach((activity) => {
         let activityList = document.createElement('li');
         let activityTitle = document.createTextNode(`Week ${activity.week}`);
 
         activityList.appendChild(activityTitle);
 
-        let numberedList = document.createElement('ol');
-
-        activity.links.forEach((link) => {
-            let myLinks = document.createElement('li'); 
-            let linkList = document.createElement('a');
-            linkList.href = link.url.startsWith('http') ? link.url : mainURL + link.url;
-            linkList.textContent = link.title;
-
-            
-            myLinks.appendChild(linkList);
-            numberedList.appendChild(myLinks);
-
-        });
+        let numberedList = displayList(activity.links);
 
         activityList.appendChild(numberedList);
         allActivities.appendChild(activityList);
         
     });
+}
+
+const displayWebActivity = (classActivities) => {
+    classActivities.forEach((classActivity) => {
+        let activityList = document.createElement('li');
+        let activityTitle = document.createTextNode(`WDD ${classActivity.wdd}`);
+
+        activityList.appendChild(activityTitle);
+
+        let numberedList = displayList(classActivity.links);
+        
+        activityList.appendChild(numberedList);
+        allActivities.appendChild(activityList);
+        
+    });
+}
+
+let displayList = (links) => {
+    let numberedList = document.createElement('ol');
+
+    links.forEach((link) => {
+        let myLinks = document.createElement('li'); 
+        let linkList = document.createElement('a');
+        
+        linkList.href = link.url.startsWith('http') ? link.url : mainURL + link.url;
+        linkList.textContent = link.title;
+        
+        myLinks.appendChild(linkList);
+        numberedList.appendChild(myLinks);
+    });
+
+    return numberedList;
 }
 
 getActivityLinks()
